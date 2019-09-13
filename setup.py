b6ffcc5254617c212b1621ebc213cb4a08d2296c
Athanasios Anastasiou <athanastasiou@gmail.com>
2019-09-13 01:35:30 +0100

    Minor changes in the setup script

--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
-from distutils.core import setup
+import sys
+
+from setuptools import setup, find_packages
 
 setup(
-    name='pywundercam',
-    version='0.1',
-    author='Athanasios Anastasiou',
-    author_email='athanastasiou@gmail.com',
-    packages=['pywundercam'],
-    scripts=[],
-    url='http://www.github.com/',
-    license='LICENSE.txt',
-    description='PyWunderCam is a Python interface to Wunder360 S1 panoramic cameras',
-    long_description=open('README.txt').read(),
-    install_requires=[
-        "Django >= 1.1.1",
-        "caldav == 0.1.4",
-    ],
-)
+    name="pywundercam",
+    version="0.0.1",
+    description="Python interface to the Wunder 360 S1 panoramic camera.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Athanasios Anastasiou",
+    author_email="athanastasiou@gmail.com",
+    zip_safe=True,
+    url="",
+    license="License :: OSI Approved :: Apache Software License",
+    packages=find_packages(exclude=("test", "test.*")),
+    keywords="image camera panoramic 360",
+    install_requires=['requests', 'pillow'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3",
+        "Topic :: Multimedia :: Graphics :: Capture :: Digital Camera",
+    ])
